e55dde1e219d0582b6fe4987cc98c755a66b46c5
Alisultan Abdullah <alisultanabdullaev@gmail.com>
2025-12-22 03:00:03 -0600

    feat: add initial PrayerEase Watch App target with basic UI and project configuration.

---
+++ PrayerEaseWatch Watch App/PrayerEaseWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrayerEaseWatchApp.swift
+//  PrayerEaseWatch Watch App
+//
+//  Created by Alisultan Abdullah on 12/22/25.
+//
+
+import SwiftUI
+
+@main
+struct PrayerEaseWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
