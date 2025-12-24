c1f71b0028f6a4222603fad75d853103abfe6cf5
Alisultan Abdullah <alisultanabdullaev@gmail.com>
2025-12-24 14:27:40 -0600

    feat: add watchOS widget target with corner complication

---
+++ WatchWidget/WatchWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WatchWidgetBundle.swift
+//  WatchWidget
+//
+//  Created by Alisultan Abdullah on 12/24/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WatchWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WatchWidget()
+        WatchWidgetControl()
+    }
+}
